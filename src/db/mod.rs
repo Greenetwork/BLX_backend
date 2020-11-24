843a43eedf4a608e1cc42d2584cdd915eaa8e96a
Elliott Minns <elliott.minns@me.com>
2020-11-24 09:31:35 -0600

    Geometry data as WKT

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -26,24 +26,28 @@ pub fn get_client() -> Pool<PostgresConnectionManager> {
 
 pub fn get_apn(identifier: i64, db: &mut Connection) -> Result<Option<APN>, Error> {
     let statement = db
-        .prepare(
-            "select * from blx_consolidated_apn where apn = $1 ",
+        .prepare(r#"
+            SELECT apn_chr, apn, objectid, agencyname, agencyuniqueid,
+            dwr_revise, region, apn_chr, county, crop2016, id, acres,
+            ST_AsText(geometry) as geometry
+            FROM blx_consolidated_apn where apn = $1
+            "#
         )?;
 
     let apn: Option<APN> = statement.query(&[&identifier])?
         .iter()
         .fold(None, |_acc, row| {
-            let apn_chr: String = row.get("apn_chr");
-            let apn: i64 = row.get("apn");
-            let geometry: Vec<u8> = row.get("geometry");
-            let object_id: i64 = row.get("objectid");
-            let agency_name: String = row.get("agencyname");
-            let agency_unique_id: String = row.get("agencyuniqueid");
-            let dwr_revise: String = row.get("dwr_revise");
-            let region: String = row.get("region");
-            let acres: f64 = row.get("acres");
-            let county: String = row.get("county");
-            let crop2016: String = row.get("crop2016");
+            let apn_chr: Option<String> = row.get("apn_chr");
+            let apn: Option<i64> = row.get("apn");
+            let geometry: Option<String> = row.get("geometry");
+            let object_id: Option<i64> = row.get("objectid");
+            let agency_name: Option<String> = row.get("agencyname");
+            let agency_unique_id: Option<i64> = row.get("agencyuniqueid");
+            let dwr_revise: Option<String> = row.get("dwr_revise");
+            let region: Option<String> = row.get("region");
+            let acres: Option<f64> = row.get("acres");
+            let county: Option<String> = row.get("county");
+            let crop2016: Option<String> = row.get("crop2016");
             let id: i32 = row.get("id");
 
             Some(APN {
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -4,17 +4,17 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct APN {
-    pub apn_chr: String,
-    pub apn: i64,
-    pub geometry: Vec<u8>,
-    pub object_id: i64,
-    pub agency_name: String,
-    pub agency_unique_id: String,
-    pub dwr_revise: String,
-    pub region: String,
-    pub acres: f64,
-    pub county: String,
-    pub crop2016: String,
+    pub apn_chr: Option<String>,
+    pub apn: Option<i64>,
+    pub geometry: Option<String>,
+    pub object_id: Option<i64>,
+    pub agency_name: Option<String>,
+    pub agency_unique_id: Option<i64>,
+    pub dwr_revise: Option<String>,
+    pub region: Option<String>,
+    pub acres: Option<f64>,
+    pub county: Option<String>,
+    pub crop2016: Option<String>,
     pub id: i32,
 }
 
