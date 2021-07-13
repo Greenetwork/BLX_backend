de34261ae6846f6e1901c4756dd3e67dc9b15dcf
Joe Wagner <n.joseph.wagner@gmail.com>
2021-07-13 12:36:15 -0600

    WIP not running add ability to get list of APNs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -70,6 +70,53 @@ pub fn get_apn(identifier: i64, db: &mut Connection) -> Result<Option<APN>, Erro
     Ok(apn)
 }
 
+pub fn get_apn_list(id_list: Vec<i64>, db: &mut Connection) -> Result<Option<APN>, Error> {
+    let apn_list = id_list.iter().join(", ");
+
+    let statement = db
+        .prepare(format!(r#"
+            SELECT apn_chr, apn, objectid, agencyname, agencyuniqueid,
+            dwr_revise, region, apn_chr, county, crop2016, id, acres,
+            ST_AsText(geometry) as geometry
+            FROM blx_consolidated_apn WHERE apn IN ({apn_list})
+            "#, apn_list = apn_list)
+        )?;
+
+    let apn: Option<APN> = statement.query(&[&identifier])?
+        .iter()
+        .fold(None, |_acc, row| {
+            let apn_chr: Option<String> = row.get("apn_chr");
+            let apn: Option<i64> = row.get("apn");
+            let geometry: Option<String> = row.get("geometry");
+            let object_id: Option<i64> = row.get("objectid");
+            let agency_name: Option<String> = row.get("agencyname");
+            let agency_unique_id: Option<i64> = row.get("agencyuniqueid");
+            let dwr_revise: Option<String> = row.get("dwr_revise");
+            let region: Option<String> = row.get("region");
+            let acres: Option<f64> = row.get("acres");
+            let county: Option<String> = row.get("county");
+            let crop2016: Option<String> = row.get("crop2016");
+            let id: i32 = row.get("id");
+
+            Some(APN {
+                apn_chr,
+                apn,
+                geometry,
+                object_id,
+                agency_name,
+                agency_unique_id,
+                dwr_revise,
+                region,
+                acres,
+                county,
+                crop2016,
+                id,
+            })
+        });
+
+    Ok(apn)
+}
+
 pub fn get_apn_for_chain(identifier: i64, db: &mut Connection) -> Result<Option<APN_CHAIN>, Error> {
     let statement = db
         .prepare(r#"
